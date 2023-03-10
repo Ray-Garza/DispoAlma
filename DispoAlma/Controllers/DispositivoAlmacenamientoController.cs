2af36efcfd86c35f76ce52e2751a711465d781d0
Ray-Garza <raygarzagarza@gmail.com>
2023-03-09 21:23:56 -0600

    actualización de la base de datos de la actividad 1

---
+++ DispoAlma/Controllers/DispositivoAlmacenamientoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DispoAlma.Entidades;
+
+namespace DispoAlma.Controllers
+{
+    [ApiController]
+    [Route("api/dispositivos")]
+    public class DispositivoAlmacenamientoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<DispositivoAlmacenamiento>> Get()
+        {
+            return new List<DispositivoAlmacenamiento>(){
+                new DispositivoAlmacenamiento() { id_dispositivo = 1, 
+                                                  nombre_dispositivo = "Disco SSD Acer NVMe", 
+                                                  almacenamiento_dispositivo = 1000, 
+                                                  descripcion_dispositivo= "SSD Acer FA100 NVMe, 1TB, PCI Express 3.0, M.2",
+                                                  velocidad_escritura=2700,
+                                                  velocidad_lectura=3300,
+                                                  cantidad = 200},
+
+                new DispositivoAlmacenamiento() { id_dispositivo = 2, nombre_dispositivo = "Disco SSD Acer SATA",
+                                                  almacenamiento_dispositivo = 960,
+                                                  descripcion_dispositivo= "SSD Acer SA100, 960GB, SATA III, 2.5",
+                                                  velocidad_escritura=560,
+                                                  velocidad_lectura=500,
+                                                  cantidad = 150},
+            };
+        }
+    }
+}
--- DispoAlma/Entidades/DispositivoAlmacenamiento.cs
+++ DispoAlma/Entidades/DispositivoAlmacenamiento.cs
@@ -4,5 +4,11 @@
     {
         public int id_dispositivo { get; set; }
         public string nombre_dispositivo { get; set; }
+
+        public int almacenamiento_dispositivo { get; set; }
+        public string descripcion_dispositivo { get; set; }
+        public int velocidad_escritura { get; set; }
+        public int velocidad_lectura { get; set; } 
+        public int cantidad { get;set; }
     }
 }
